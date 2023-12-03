41cad4223b206652a787efcbea4681a3ac58a6ed
devcodigo <developer@codigo.cz>
2023-12-03 22:05:50 +0100

    les3fin

--- app/Filament/Resources/LeadSourceResource.php
+++ app/Filament/Resources/LeadSourceResource.php
@@ -18,7 +18,9 @@ class LeadSourceResource extends Resource
 {
     protected static ?string $model = LeadSource::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationGroup = 'Settings';
+
+    
 
     public static function form(Form $form): Form
     {
