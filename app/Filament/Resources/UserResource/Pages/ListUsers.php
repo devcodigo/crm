b754444546d5d8722cf036605a3660e6941e3e8e
devcodigo <developer@codigo.cz>
2023-12-12 20:09:45 +0100

    les13

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -3,8 +3,14 @@
 namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
+use App\Mail\TeamInvitationMail;
+use App\Models\Invitation;
 use Filament\Actions;
+use Filament\Forms\Components\TextInput;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Support\Facades\Mail;
+use Symfony\Contracts\Service\Attribute\Required;
 
 class ListUsers extends ListRecords
 {
@@ -14,6 +20,22 @@ class ListUsers extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+            Actions\Action::make('inviteUser')
+                ->form([
+                TextInput::make('email')
+                    ->email()
+                    ->required()
+            ])
+                ->action(function($data) {
+                    $invitation = Invitation::create(['email'=> $data['email']]);
+
+                Mail::to($invitation->email)->send(new TeamInvitationMail($invitation));
+
+                Notification::make('invitedSucces')
+                ->body('User invited succesfuly')
+                ->success()->send();
+
+                }),
         ];
     }
 }
